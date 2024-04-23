90e44ea428dc8ff2ca5f3b1b486639619a74ad64
HerreraMatias <00mh08@gmail.com>
2024-04-23 09:45:57 -0300

    Lectura VBatt en ADC_IN OK! :)
    Lee bien la tension de la bateria en el pin analogico

--- src/battery_functions.h
+++ src/battery_functions.h
@@ -1,46 +1,46 @@
 #include "Arduino.h"
 
-/**
- * ADC1_CH0 is used to read the lithium battery voltage, the voltage of the lithium battery is:
- * VADC_IN1 = 100 / (100+390) * VBAT
- * Se encuentra en GPIO 1
- * 12 1 I/O GPIO1, ADC1_CH03
-, TOUCH1, Read VBAT Voltage
-
- **/
-
-#define PIN_ADC_IN 1
-#define PIN_ADC_CTRL 37
-
 float battery_value = -1;
-int adc1_value = -1;
+
+
+#define VBAT_Read 1
+#define ADC_Ctrl 37
+
 
 void battery_config()
 {
-    pinMode(PIN_ADC_IN, INPUT);
-    pinMode(PIN_ADC_CTRL, OUTPUT);
-    digitalWrite(PIN_ADC_CTRL, LOW);
+    pinMode(ADC_Ctrl, OUTPUT);
+    pinMode(VBAT_Read, INPUT);
+    adcAttachPin(VBAT_Read);
+    analogReadResolution(12);
+
 }
 
 void battery_read()
 {
-    static int x = 0;
-    //x == 0 ? x = 1 : x = 0;
+    // ADC resolution
+    const int resolution = 12;
+    const int adcMax = pow(2, resolution) - 1;
+    const float adcMaxVoltage = 3.3;
+    // On-board voltage divider
+    const int R1 = 390;
+    const int R2 = 100;
+    // Calibration measurements
+    const float measuredVoltage = 4.2;
+    const float reportedVoltage = 4.095;
+    // Calibration factor
+    //const float factor = (adcMaxVoltage / adcMax) * ((R1 + R2) / (float)R2) * (measuredVoltage / reportedVoltage);
+    const float factor = (adcMaxVoltage / adcMax) * ((R1 + R2) / (float)R2);
+    digitalWrite(ADC_Ctrl, LOW);
+    delay(100);
+    int analogValue = analogRead(VBAT_Read);
+    digitalWrite(ADC_Ctrl, HIGH);
 
-    if (x)
-    {
-        digitalWrite(PIN_ADC_CTRL, LOW);
-        adc1_value = analogRead(PIN_ADC_IN);
-        battery_value = (100 / (100 + 390)) * adc1_value;
-        Serial.print("\nBatery voltage 1: " + String(battery_value) + " _ ADC: " + String(adc1_value));
-        x = 0;
-    }
-    else
-    {
-        x = 1;
-        digitalWrite(PIN_ADC_CTRL, HIGH);
-        adc1_value = analogRead(PIN_ADC_IN);
-        battery_value = (100 / (100 + 390)) * adc1_value;
-        Serial.print("\nBatery voltage 2: " + String(battery_value) + " _ ADC: " + String(adc1_value));
-    }
+    float floatVoltage = factor * analogValue;
+    uint16_t voltage = (int)(floatVoltage * 1000.0);
+
+    battery_value = floatVoltage;
+
+    
+    Serial.print("Battery: " + String(battery_value) + " V, Vin: " + String(voltage) + " mV");
 }
